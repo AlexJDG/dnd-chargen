41c3b129326a6931e040f52ffdf2a7e85644d7eb
Alex Granatowicz <alex.g@oneupsales.co.uk>
2024-08-13 11:21:31 +0100

    adding prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { createTamagui, TamaguiProvider } from "tamagui";
+import defaultConfig from "@tamagui/config/v3";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const config = createTamagui(defaultConfig);
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <TamaguiProvider config={config}>
+            <App />
+        </TamaguiProvider>
+    </StrictMode>,
+);
