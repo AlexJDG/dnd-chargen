18a95e9839bf07d36ec4b4b4f576fc54645b611f
Alex Granatowicz <alex.g@oneupsales.co.uk>
2024-08-13 11:23:24 +0100

    removing tamagui

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { createTamagui, TamaguiProvider } from "tamagui";
-import defaultConfig from "@tamagui/config/v3";
-
-const config = createTamagui(defaultConfig);
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <TamaguiProvider config={config}>
-            <App />
-        </TamaguiProvider>
+        <App />
     </StrictMode>,
 );
